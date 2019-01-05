75e65b25d999fc7254003b197eed4d069c6335f1
Pravin <526473+gb96@users.noreply.github.com>
2019-01-05 22:49:58 +0530

    git rebase --continue git pushComplete working of three option of WFL project...

---
+++ app/src/main/java/wfl/pravin/wayforlife/ImagesActivity.java
@@ -0,0 +1,13 @@
+package wfl.pravin.wayforlife;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ImagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+    }
+}
