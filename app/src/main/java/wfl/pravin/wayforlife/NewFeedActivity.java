b089f942d98e5a5d8509a1c64fec61779d252cfa
Nitin <canvas.nv@gmail.com>
2019-01-07 15:15:42 +0530

    dicussions fetched and shown

--- app/src/main/java/wfl/pravin/wayforlife/NewFeedActivity.java
+++ app/src/main/java/wfl/pravin/wayforlife/NewFeedActivity.java
@@ -7,13 +7,25 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import wfl.pravin.wayforlife.adapter.DiscussionAdapter;
 import wfl.pravin.wayforlife.models.Discussion;
 
 public class NewFeedActivity extends AppCompatActivity {
@@ -24,13 +36,37 @@ public class NewFeedActivity extends AppCompatActivity {
     private static final String USER_CITY = "Rupnagar";
     private static final String DISCUSSIONS = "discussions";
 
+    RecyclerView mRecyclerView;
+    private DatabaseReference mDiscussionReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_feed);
 
+        mRecyclerView = findViewById(R.id.discussion_rv);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
+        mDiscussionReference = FirebaseDatabase.getInstance().getReference(DISCUSSIONS).child(USER_CITY);
+        mDiscussionReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Discussion> discussionList = new ArrayList<>();
+                for (DataSnapshot discussionSnapshot : dataSnapshot.getChildren()) {
+                    Discussion d = discussionSnapshot.getValue(Discussion.class);
+                    discussionList.add(d);
+                }
+                DiscussionAdapter discussionAdapter = new DiscussionAdapter(discussionList);
+                mRecyclerView.setAdapter(discussionAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void openComplaintsActivity(View view) {
@@ -65,26 +101,21 @@ public class NewFeedActivity extends AppCompatActivity {
         if (titleEditText.getText().toString().trim().length() == 0) {
             Snackbar.make(findViewById(R.id.discussion_rv), "Title can't be empty", Snackbar.LENGTH_SHORT).show();
         }
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseDatabase.getReference()
-                .child(DISCUSSIONS).child(USER_CITY).push()
-                .setValue(new Discussion(
-                        titleEditText.getText().toString(),
-                        USER_ID,
-                        USER_NAME,
-                        null
-                ))
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Snackbar.make(findViewById(R.id.discussion_rv), "Discussion added", Snackbar.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Snackbar.make(findViewById(R.id.discussion_rv), "Error adding discussion", Snackbar.LENGTH_SHORT).show();
-                    }
-                });
+        mDiscussionReference.push().setValue(new Discussion(
+                titleEditText.getText().toString(),
+                USER_ID,
+                USER_NAME,
+                null
+        )).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Snackbar.make(findViewById(R.id.discussion_rv), "Discussion added", Snackbar.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Snackbar.make(findViewById(R.id.discussion_rv), "Error adding discussion", Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/wfl/pravin/wayforlife/adapter/DiscussionAdapter.java
+++ app/src/main/java/wfl/pravin/wayforlife/adapter/DiscussionAdapter.java
@@ -1,14 +1,83 @@
 package wfl.pravin.wayforlife.adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class DiscussionAdapter {
+import java.util.List;
 
-    public class DiscussionViewHolder extends RecyclerView.ViewHolder {
+import wfl.pravin.wayforlife.R;
+import wfl.pravin.wayforlife.models.Discussion;
 
-        public DiscussionViewHolder(View itemView) {
+public class DiscussionAdapter extends RecyclerView.Adapter<DiscussionAdapter.DiscussionViewHolder> {
+    private static final String TAG = "Nitin-DiscussionAdapter";
+
+    private List<Discussion> discussionList;
+
+    public DiscussionAdapter(List<Discussion> discussions) {
+        this.discussionList = discussions;
+    }
+
+    @NonNull
+    @Override
+    public DiscussionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_discussion, parent, false);
+        return new DiscussionViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final DiscussionViewHolder holder, int position) {
+        Discussion discussion = discussionList.get(position);
+        holder.title.setText(discussion.getTitle());
+        holder.userName.setText(String.format("-By %s", discussion.getUserName()));
+        holder.sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: send comment=" + holder.newComment.getText().toString());
+            }
+        });
+        holder.commentsToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = holder.commentsToggle.getText().toString();
+                if (value.equals("Show comments")) {
+                    //TODO : show the comments
+                    holder.commentsToggle.setText("Hide comments");
+                } else {
+                    //TODO : hide the comments
+                    holder.commentsToggle.setText("Show comments");
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return discussionList.size();
+    }
+
+    class DiscussionViewHolder extends RecyclerView.ViewHolder {
+        TextView title, userName, commentsToggle;
+        ListView comments;
+        EditText newComment;
+        ImageButton sendButton;
+
+        DiscussionViewHolder(View itemView) {
             super(itemView);
+
+            title = itemView.findViewById(R.id.discussion_title);
+            userName = itemView.findViewById(R.id.discussion_username);
+            commentsToggle = itemView.findViewById(R.id.comments_visibility_toggle);
+            comments = itemView.findViewById(R.id.discussion_comments_lv);
+            newComment = itemView.findViewById(R.id.discussion_new_comment);
+            sendButton = itemView.findViewById(R.id.discussion_send_btn);
         }
     }
 }
